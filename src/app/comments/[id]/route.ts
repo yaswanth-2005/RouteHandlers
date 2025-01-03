80692f9b1cd31859261f5232348f62d8178f6745
Yaswanth Varada <yaswanthvarada20@gmail.com>
2025-01-03 20:38:14 +0530

    baisc Route HTTP GET, POST, etc.. written

--- src/app/comments/[id]/route.ts
+++ src/app/comments/[id]/route.ts
@@ -1,10 +1,13 @@
-import { comment } from "postcss";
+import { redirect } from "next/navigation";
 import { comments } from "../data";
 
 export async function GET(
   _request: Request,
   { params }: { params: { id: string } }
 ) {
+  if (parseInt(params.id) > comments.length) {
+    redirect("/comments");
+  }
   const comment = comments.find(
     (comment) => comment.id === parseInt(params.id)
   );
